8240e958d63dc909def08882d0e61bbd1b440063
Md. Shahidul Islam <shanto57575@gmail.com>
2025-07-17 19:00:57 +0600

    part-4 complete

--- src/app/config/passport.ts
+++ src/app/config/passport.ts
@@ -1,3 +1,4 @@
+import bcryptjs from "bcryptjs";
 /* eslint-disable @typescript-eslint/no-explicit-any */
 import passport from "passport";
 import {
@@ -8,6 +9,49 @@ import {
 import { envVars } from "./env";
 import { User } from "../modules/user/user.model";
 import { Role } from "../modules/user/user.interface";
+import { Strategy as LocalStrategy } from "passport-local";
+
+passport.use(
+  new LocalStrategy(
+    {
+      usernameField: "email",
+      passwordField: "password",
+    },
+    async (email: string, password: string, done) => {
+      try {
+        const isUserExist = await User.findOne({ email });
+
+        if (!isUserExist) {
+          return done(null, false, { message: "User does not exist!" });
+        }
+
+        const isGoogleAuthenticated = isUserExist.auths.some(
+          (providerObject) => providerObject.provider === "google"
+        );
+
+        if (isGoogleAuthenticated && !isUserExist.password) {
+          return done(null, false, {
+            message:
+              "you have authenticated through google login! if you want to login through credentials at first login with google then set a password",
+          });
+        }
+
+        const isPasswordMatched = await bcryptjs.compare(
+          password,
+          isUserExist.password as string
+        );
+        if (!isPasswordMatched) {
+          return done(null, false, { message: "Invalid credentials!" });
+        }
+
+        return done(null, isUserExist);
+      } catch (error) {
+        console.log(error);
+        done(error);
+      }
+    }
+  )
+);
 
 passport.use(
   new GoogleStrategy(
--- src/app/modules/auth/auth.service.ts
+++ src/app/modules/auth/auth.service.ts
@@ -1,44 +1,40 @@
 /* eslint-disable @typescript-eslint/no-non-null-assertion */
 import httpStatus from "http-status-codes";
-import { IUser } from "../user/user.interface";
 import { User } from "../user/user.model";
 import AppError from "../../errorHelpers/AppError";
-import {
-  createNewAccessTokenWithRefreshToken,
-  createUserTokens,
-} from "../../utils/userTokens";
+import { createNewAccessTokenWithRefreshToken } from "../../utils/userTokens";
 import { JwtPayload } from "jsonwebtoken";
 import bcryptjs from "bcryptjs";
 import { envVars } from "../../config/env";
 
-const credentialsLoginService = async (payload: Partial<IUser>) => {
-  const { email, password } = await payload;
-  const isUserExists = await User.findOne({ email });
+// const credentialsLoginService = async (payload: Partial<IUser>) => {
+//   const { email, password } = await payload;
+//   const isUserExists = await User.findOne({ email });
 
-  if (!isUserExists) {
-    throw new AppError(httpStatus.BAD_REQUEST, "user does not exist");
-  }
+//   if (!isUserExists) {
+//     throw new AppError(httpStatus.BAD_REQUEST, "user does not exist");
+//   }
 
-  const isPasswordMatched = await bcryptjs.compare(
-    password as string,
-    isUserExists.password as string
-  );
+//   const isPasswordMatched = await bcryptjs.compare(
+//     password as string,
+//     isUserExists.password as string
+//   );
 
-  if (!isPasswordMatched) {
-    throw new AppError(httpStatus.BAD_REQUEST, "Invalid credentials");
-  }
+//   if (!isPasswordMatched) {
+//     throw new AppError(httpStatus.BAD_REQUEST, "Invalid credentials");
+//   }
 
-  const userTokens = createUserTokens(isUserExists);
+//   const userTokens = createUserTokens(isUserExists);
 
-  // eslint-disable-next-line @typescript-eslint/no-unused-vars
-  const { password: pass, ...rest } = isUserExists.toObject();
+//   // eslint-disable-next-line @typescript-eslint/no-unused-vars
+//   const { password: pass, ...rest } = isUserExists.toObject();
 
-  return {
-    accessToken: userTokens.accessToken,
-    refreshToken: userTokens.refreshToken,
-    user: rest,
-  };
-};
+//   return {
+//     accessToken: userTokens.accessToken,
+//     refreshToken: userTokens.refreshToken,
+//     user: rest,
+//   };
+// };
 
 const getNewAccessTokenService = async (refreshToken: string) => {
   const newAccessToken = await createNewAccessTokenWithRefreshToken(
@@ -75,7 +71,7 @@ const resetPassword = async (
 };
 
 export const AuthServices = {
-  credentialsLoginService,
+  // credentialsLoginService,
   getNewAccessTokenService,
   resetPassword,
 };
